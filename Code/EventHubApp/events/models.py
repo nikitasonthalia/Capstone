7c3f8d3d1c136ebe7d051cd97630ccd8c6107c9c
Amruta Kulkarni <amrutatkulkarni@gmail.com>
2017-10-31 22:37:35 -0700

    MVC

--- Code/EventHubApp/events/models.py
+++ Code/EventHubApp/events/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id = models.IntegerField
+    user_id = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=60)
+    street1 = models.CharField(max_length=30)
+    street2 = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    pin_number = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+
+    def __str__(self):  # __unicode__ on Python 2
+        return self.username
+
+class Category(models.Model):
+    category_id = models.IntegerField
+    category_name = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+    category_url = models.CharField(max_length=50)
+
+    def __str__(self):  # __unicode__ on Python 2
+        #return self.category_name
+        return '%s %s' % (self.category_name, self.category_url)
